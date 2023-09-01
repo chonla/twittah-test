c8a3ca30ea47ee6844a80ed9954d902e4fee58ab
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-09-01 22:05:04 +0700

    add login fail case

--- tests/twittah.spec.ts
+++ tests/twittah.spec.ts
@@ -7,7 +7,7 @@ test('Visit Twittah!', async ({ page }) => {
 });
 
 test.describe('Login Twittah!', () => {
-  test('Login success', async ({ page }) => {
+  test('Login สำเร็จ ต้องไปที่หน้าโปรไฟล์', async ({ page }) => {
     await page.goto('https://twittah.web.app');
 
     await page.getByTestId('login-field').fill('bancha');
@@ -18,5 +18,18 @@ test.describe('Login Twittah!', () => {
     await expect(page.getByTestId('user-profile-display-name')).toHaveText('บัญชา');
     await expect(page.getByTestId('user-profile-login-name')).toHaveText('@bancha');
     await expect(page.url()).toEqual('https://twittah.web.app/bancha');
+  });
+
+  test('Login ไม่ผ่าน เพราะรหัสผ่านไม่ถูกต้อง', async ({ page }) => {
+    await page.goto('https://twittah.web.app');
+
+    await page.getByTestId('login-field').fill('bancha');
+    await page.getByTestId('password-field').fill('not-bancha-password');
+    await page.getByTestId('login-button').click();
+
+    await expect(page.getByTestId('error-message')).toBeVisible();
+    await expect(page.getByTestId('error-message')).toHaveText('ล็อกอินหรือรหัสผ่านไม่ถูกต้อง');
+    await expect(page.getByTestId('user-profile')).not.toBeVisible();
+    await expect(page.url()).toEqual('https://twittah.web.app/');
   });
 });
