58564638f23a9896bc2b84f3f4678dd97eb9cf0a
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-09-04 21:10:22 +0700

    fix: Login business flow changes

--- tests/twittah.spec.ts
+++ tests/twittah.spec.ts
@@ -7,17 +7,17 @@ test('Visit Twittah!', async ({ page }) => {
 });
 
 test.describe('Login Twittah!', () => {
-  test('Login สำเร็จ ต้องไปที่หน้าโปรไฟล์', async ({ page }) => {
+  test('Login สำเร็จ ต้องไปที่หน้าแรก', async ({ page }) => {
     await page.goto('https://twittah.web.app');
 
     await page.getByTestId('login-field').fill('bancha');
     await page.getByTestId('password-field').fill('123456');
     await page.getByTestId('login-button').click();
 
-    await expect(page.getByTestId('user-profile')).toBeVisible();
+    await expect(page.getByTestId('current-user-profile')).toBeVisible();
     await expect(page.getByTestId('user-profile-display-name')).toHaveText('บัญชา');
     await expect(page.getByTestId('user-profile-login-name')).toHaveText('@bancha');
-    await expect(page.url()).toEqual('https://twittah.web.app/bancha');
+    await expect(page.url()).toEqual('https://twittah.web.app/home');
   });
 
   test('Login ไม่ผ่าน เพราะรหัสผ่านไม่ถูกต้อง', async ({ page }) => {
